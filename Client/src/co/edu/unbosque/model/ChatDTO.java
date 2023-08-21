749fafdb69d1294e35e1a0417106dd22d9ef94b8
Samuel Contreras Albornoz <141385127+samco1801@users.noreply.github.com>
2023-08-21 16:34:02 -0500

    Cliente del programa
    Añadí el primer boceto del proyecto cliente del programa, en donde este directamente se va comunicar con el servidor.

---
+++ Client/src/co/edu/unbosque/model/ChatDTO.java
@@ -0,0 +1,17 @@
+package co.edu.unbosque.model;
+
+import java.io.Serializable;
+
+public class ChatDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1864676271689711159L;
+
+	public ChatDTO() {
+		
+	}
+	
+	
+}
