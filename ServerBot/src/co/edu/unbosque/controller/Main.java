ff106cc560d9b5da1a0443632752e03e2f0123ca
Samuel Contreras Albornoz <141385127+samco1801@users.noreply.github.com>
2023-08-22 23:14:24 -0500

    Actualización final del Servidor
    Añadí los últimos cambios del servidor. El proyecto ya quedó terminado.

--- ServerBot/src/co/edu/unbosque/controller/Main.java
+++ ServerBot/src/co/edu/unbosque/controller/Main.java
@@ -3,7 +3,7 @@ package co.edu.unbosque.controller;
 public class Main {
 
 	public static void main(String[] args) {
-		Server server = new Server(5000);
+		Server server = new Server(9100);
 		server.start();
 	}
 
