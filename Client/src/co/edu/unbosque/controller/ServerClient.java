86e52e3b8843cf62a8cb219ad5af0a5979f646c5
Samuel Contreras Albornoz <141385127+samco1801@users.noreply.github.com>
2023-08-22 19:27:44 -0500

    Actualizacion del cliente
    Subí un nuevo codigo en donde ya se pueden enviar varias cadenas de texto escritas por consola al servidor.

--- Client/src/co/edu/unbosque/controller/ServerClient.java
+++ Client/src/co/edu/unbosque/controller/ServerClient.java
@@ -1,9 +1,9 @@
 package co.edu.unbosque.controller;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -15,43 +15,45 @@ public class ServerClient extends Thread {
 	private Socket socket;
 	private ServerSocket server;
 	private Scanner sc;
-	private ObjectOutputStream out;
-	private ObjectInputStream in;
+	private DataOutputStream out;
+	private DataInputStream in;
 	private String address;
 	private int port;
 
 	public ServerClient(String address, int port) {
+
 		this.socket = null;
 		this.server = null;
 		this.sc = new Scanner(System.in);
 		this.out = null;
 		this.address = address;
 		this.port = port;
-
 	}
 
 	@Override
 	public void run() {
-		String line = "";
 
-		while (!line.equals("Over")) {
+		String word = "";
 
+		while (!word.equalsIgnoreCase("Terminado")) {
 			try {
 				this.socket = new Socket(this.address, this.port);
-				System.out.println("Connected");
+				System.out.println("Conectado al servidor");
+				System.out.println("Digite un mensaje:");
 
-				this.out = new ObjectOutputStream(socket.getOutputStream());
+				this.out = new DataOutputStream(socket.getOutputStream());
 
-				ChatDTO chat = new ChatDTO("Hola");
-				this.out.writeObject(chat);
+				word = sc.nextLine();
+				this.out.writeUTF(word);
 				this.out.close();
 				this.socket.close();
 				this.server = new ServerSocket(this.port + 1);
 				this.socket = server.accept();
-				System.out.println("Received message:");
-				this.in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+				System.out.println("Mensaje recibido! \n");
+				this.in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 				System.out.println(in.readUTF());
 				this.in.close();
+				this.out.close();
 				this.server.close();
 			} catch (IOException i) {
 				System.out.println(i);
@@ -65,4 +67,5 @@ public class ServerClient extends Thread {
 		}
 
 	}
+
 }
